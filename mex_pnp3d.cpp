7b2b492305878d8f9115aa58497e720667b63989
leeneil <lishantreasure@gmail.com>
2018-05-16 12:34:47 -0700

    mex for 3d pnp

---
+++ mex_pnp3d.cpp
@@ -0,0 +1,349 @@
+#include <mex.h>
+#include <matrix.h>
+#include <iostream>
+
+using namespace std;
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+
+	double *C;
+	double *Ez, *Er, *Eq;
+	double *Jz, *Jr, *Jq;
+
+	double *z, *d_m;
+
+	double *dqq, *dx, *dt;
+	double *R;
+	double *a, *b, *c;
+
+	double *bulk;
+	double *amo_z, *amo_r;
+	double *slp;
+
+	double *n_display;
+
+	int m0, n0, l0, m1, n1, l1, m2, n2, l2, m3, n3, l3;
+	mwSize *dim0, *dim1, *dim2, *dim3;
+
+	double *CNew;
+	double *EzNew, *ErNew, *EqNew;
+	double *JzNew, *JrNew, *JqNew;
+
+	/* parse input arguments */
+
+	C  = mxGetPr(prhs[0]);
+	dim0 = (mwSize*)mxGetDimensions(prhs[0]);
+
+	Ez = mxGetPr(prhs[1]);
+	Er = mxGetPr(prhs[2]);
+	Eq = mxGetPr(prhs[3]);
+
+	Jz = mxGetPr(prhs[4]);
+	dim1 = (mwSize*)mxGetDimensions(prhs[4]);
+	Jr = mxGetPr(prhs[5]);
+	dim2 = (mwSize*)mxGetDimensions(prhs[5]);
+	Jq = mxGetPr(prhs[6]);
+	dim3 = (mwSize*)mxGetDimensions(prhs[6]);
+
+
+	m0 = (int)dim0[0];
+ 	n0 = (int)dim0[1];
+ 	l0 = (int)dim0[2];
+ 	m1 = (int)dim1[0];
+	n1 = (int)dim1[1];
+	l1 = (int)dim0[2];
+ 	m2 = (int)dim2[0];
+	n2 = (int)dim2[1];
+	l2 = (int)dim2[2];
+	m3 = (int)dim3[0];
+	n3 = (int)dim3[1];
+	l3 = (int)dim3[2];
+
+	// cout << dim0[0] << " " << dim0[1] << " " << dim0[2] << endl;
+	// cout << dim1[0] << " " << dim1[1] << " " << dim1[2] << endl;
+	// cout << dim2[0] << " " << dim2[1] << " " << dim2[2] << endl;
+
+	z   = mxGetPr(prhs[7]);
+	d_m = mxGetPr(prhs[8]);
+
+
+  dqq = mxGetPr(prhs[9]);
+	dx = mxGetPr(prhs[10]);
+	dt = mxGetPr(prhs[11]);
+
+	R = mxGetPr(prhs[12]);
+
+	a = mxGetPr(prhs[13]);
+	b = mxGetPr(prhs[14]);
+	c = mxGetPr(prhs[15]);
+
+	bulk = mxGetPr(prhs[16]);
+
+	amo_z = mxGetPr(prhs[17]);
+	amo_r = mxGetPr(prhs[18]);
+
+	slp = mxGetPr(prhs[19]);
+
+	n_display  = mxGetPr(prhs[20]);
+
+
+
+	/* create output arguments */
+    plhs[0] = mxCreateNumericArray(4, dim0, mxDOUBLE_CLASS, mxREAL);
+    CNew = mxGetPr(plhs[0]);
+    plhs[1] = mxCreateNumericArray(3, dim1, mxDOUBLE_CLASS, mxREAL);
+    EzNew = mxGetPr(plhs[1]);
+    plhs[2] = mxCreateNumericArray(3, dim2, mxDOUBLE_CLASS, mxREAL);
+    ErNew = mxGetPr(plhs[2]);
+    plhs[3] = mxCreateNumericArray(3, dim3, mxDOUBLE_CLASS, mxREAL);
+    EqNew = mxGetPr(plhs[3]);
+    plhs[4] = mxCreateNumericArray(4, dim1, mxDOUBLE_CLASS, mxREAL);
+    JzNew = mxGetPr(plhs[4]);
+    plhs[5] = mxCreateNumericArray(4, dim2, mxDOUBLE_CLASS, mxREAL);
+    JrNew = mxGetPr(plhs[5]);
+    plhs[6] = mxCreateNumericArray(4, dim3, mxDOUBLE_CLASS, mxREAL);
+    JqNew = mxGetPr(plhs[6]);
+
+
+
+    for (int t = 0; t < (int)*n_display; t++) {
+
+    	/* update Ez */
+
+    	for (int u = 0; u < m1; u++) {
+    		for (int v = 0; v < n1; v++) {
+    			for (int w = 0; w < l1; w++) {
+    				for (int k = 0; k < 3; k++) {
+    					Ez[ u + v*m1 + w*m1*n1 ] -=  *a * z[k] * Jz[ u + v*m1 + w*m1*n1 + k*m1*n1*l1 ];
+    				}
+    			}
+    		}
+    	}
+
+    	/* update Er */
+
+    	for (int u = 0; u < m2; u++) {
+    		for (int v = 0; v < n2; v++) {
+    			for (int w = 0; w < l2; w++) {
+    				for (int k = 0; k < 3; k++) {
+    					Er[ u + v*m2 + w*m2*n2 ] -=  *a * z[k] * Jr[ u + v*m2 + w*m2*n2 + k*m2*n2*l2 ];
+    				}
+    			}
+    		}
+    	}
+
+    	/* update Eq */
+
+    	for (int u = 0; u < m3; u++) {
+    		for (int v = 0; v < n3; v++) {
+    			for (int w = 0; w < l3; w++) {
+    				for (int k = 0; k < 3; k++) {
+    					Eq[ u + v*m2 + w*m2*n2 ] -=  *a * z[k] * Jq[ u + v*m2 + w*m2*n2 + k*m2*n2*l2 ];
+    				}
+    			}
+    		}
+    	}
+
+    	/* update Jz */
+
+    	for (int u = 1; u < (m1-1); u++) {
+    		for (int v = 0; v < n1; v++) {
+    			for (int w = 0; w < l1; w++) {
+						for (int k = 0; k < 3; k++) {
+							Jz[ u + v*m1 + w*m1*n1 + k*m1*n1*l1 ]
+								= 0.5 * *b * d_m[k] * z[k]
+	    				  * ( C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ] + C[ (u-1) + v*m0 + w*m0*n0 + k*m0*n0*l0 ] )
+								* Ez[ u + v*m1 + w*m1*n1 ];
+	    				Jz[ u + v*m1 + w*m1*n1 + k*m1*n1*l1 ]
+								-= d_m[k] / *dx
+								* ( C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ] - C[ (u-1) + v*m0 + w*m0*n0 + k*m0*n0*l0 ] );
+						}
+    			}
+    		}
+    	}
+
+			/* update Jr */
+
+    	for (int u = 0; u < m2; u++) {
+    		for (int v = 0; v < (n2-1); v++) {
+    			for (int w = 0; w < l2; w++) {
+						for (int k = 0; k < 3; k++) {
+							Jr[ u + v*m2 + w*m2*n2 + k*m2*n2*l2 ]
+								= 0.5 * *b * d_m[k] * z[k]
+	    				  * ( C[ u + (v+1)*m0 + w*m0*n0 + k*m0*n0*l0 ] + C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ] )
+								* Er[ u + v*m2 + w*m2*n2 ];
+	    				Jr[ u + v*m2 + w*m2*n2 + k*m2*n2*l2 ]
+								-= d_m[k] / *dx
+								* ( C[ u + (v+1)*m0 + w*m0*n0 + k*m0*n0*l0 ] - C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ] );
+						}
+    			}
+    		}
+    	}
+
+			/* update Jq */
+
+    	for (int u = 0; u < m3; u++) {
+    		for (int v = 0; v < n3; v++) {
+					for (int k = 0; k < 3; k++) {
+    				for (int w = 1; w < (l3-1); w++) {
+							Jq[ u + v*m3 + w*m3*n3 + k*m3*n3*l3 ]
+								= 0.5 * *b * d_m[k] * z[k]
+	    				  * ( C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ] + C[ u + v*m0 + (w-1)*m0*n0 + k*m0*n0*l0 ] )
+								* Eq[ u + v*m3 + w*m3*n3 ];
+	    				Jq[ u + v*m3 + w*m3*n3 + k*m3*n3*l3 ]
+								-= d_m[k] / *dx
+								* ( C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ] - C[ u + v*m0 + (w-1)*m0*n0 + k*m0*n0*l0 ] );
+						} // end for-w
+						// Jq BC-1
+						int w = 0;
+						Jq[ u + v*m3 + w*m3*n3 + k*m3*n3*l3 ]
+							= 0.5 * *b * d_m[k] * z[k]
+							* ( C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ] + C[ u + v*m0 + (l0-1)*m0*n0 + k*m0*n0*l0 ] )
+							* Eq[ u + v*m3 + w*m3*n3 ];
+						Jq[ u + v*m3 + w*m3*n3 + k*m3*n3*l3 ]
+							-= d_m[k] / *dx
+							* ( C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ] - C[ u + v*m0 + (l0-1)*m0*n0 + k*m0*n0*l0 ] );
+					  // Jq BC-2
+						w = l3-1;
+						Jq[ u + v*m3 + w*m3*n3 + k*m3*n3*l3 ]
+							= 0.5 * *b * d_m[k] * z[k]
+							* ( C[ u + v*m0 + 0*m0*n0 + k*m0*n0*l0 ] + C[ u + v*m0 + (w-1)*m0*n0 + k*m0*n0*l0 ] )
+							* Eq[ u + v*m3 + w*m3*n3 ];
+						Jq[ u + v*m3 + w*m3*n3 + k*m3*n3*l3 ]
+							-= d_m[k] / *dx
+							* ( C[ u + v*m0 + 0*m0*n0 + k*m0*n0*l0 ] - C[ u + v*m0 + (w-1)*m0*n0 + k*m0*n0*l0 ] );
+    			} // end for-k
+    		} // end for-v
+    	} // end for-u
+
+    	/* diffusive BCs */
+
+    	for (int v = 0; v < n1; v++) {
+    		for (int w = 0; w < l1; w++) {
+					for (int k = 0; k < 3; k++) {
+						Jz[ 0 + v*m1 + w*m1*n1 + k*m1*n1*l1 ]
+							= -d_m[k] / *dx
+							* ( C[ 0 + v*m0 + w*m0*n0 + k*m0*n0*l0 ] - bulk[ 0 + v*1 + w*1*n0 + k*1*n0*l0 ] );
+					}
+    		}
+    	}
+
+    	/* Neumann BCs */
+
+    	// cout << slp[0] << " " << slp[1] << " " << slp[2] << " " << slp[3] << endl;
+
+			for (int k = 0; k < 3; k++) {
+	    	for (int w = 0; w < l1; w++) {
+
+	    		// vertical walls
+	    		for (int u = ((int)slp[0]-1); u < (int)slp[1]; u++) {
+	    			Jr[ u + ((int)slp[2]-2)*m2 + w*m2*n2 + k*m2*n2*l2 ] = 0;
+	    		}
+
+	    		// horizontal surfaces
+	    		for (int v = ((int)slp[2]-1); v < n1; v++) {
+	    			Jz[ ((int)slp[0]-1) + v*m1 + w*m1*n1 + k*m1*n1*l1 ] = 0;
+	    			Jz[ (int)slp[1] + v*m1 + w*m1*n1 + k*m1*n1*l1 ] = 0;
+	    		}
+	    	}
+			}
+
+    	/* update C */
+
+			for (int k = 0; k < 3; k++) {
+				for (int u = 0; u < m0; u++) {
+					for (int w = 0; w < l0; w++) {
+						for (int v = 0; v < n0; v++) {
+							// Jz
+							C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ]
+								-= *dt / *dx
+								* ( Jz[ (u+1) + v*m1 + w*m1*n1 + k*m1*n1*l1 ] - Jz[ u + v*m1 + w*m1*n1 + k*m1*n1*l1 ] );
+							if (v > 0) {
+								// Jr-1
+								C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ]
+									-= *dt / *dx
+									* ( Jr[ u + v*m2 + w*m2*n2 + k*m2*n2*l2 ] - Jr[ u + (v-1)*m2 + w*m2*n2 + k*m2*n2*l2 ] );
+								// Jr-2
+								C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ]
+									-= 0.5 * *dt
+									/ R[ u + v*m0 + (w+1)*m0*n0 ]
+									* ( Jr[ u + v*m2 + w*m2*n2 + k*m2*n2*l2 ] + Jr[ u + (v-1)*m2 + w*m2*n2 + k*m2*n2*l2 ] );
+								// Jq
+								C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ]
+									-= *dt / *dqq
+									/ R[ u + v*m0 + (w+1)*m0*n0 ]
+									* ( Jq[ u + v*m3 + (w+1)*m3*n3 + k*m3*n3*l3 ] - Jq[ u + v*m3 + w*m3*n3 + k*m3*n3*l3 ] );
+							} else {
+                                C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ]
+                                    -= *dt / *dx
+									* 2 * Jr[ u + v*m2 + w*m2*n2 + k*m2*n2*l2 ];
+                            }
+
+						}
+						// special case: r = 0
+						C[ u + 0*m0 + w*m0*n0 + k*m0*n0*l0 ]
+							-= 2 * *dt / *dx
+							* Jr[ u + 0*m0 + w*m0*n0 + k*m0*n0*l0 ];
+					}
+				}
+			}
+
+    	/* reaction */
+
+			for (int w = 0; w < l0; w++) {
+				C[ ((int)*amo_z-1) + ((int)*amo_r-1) * m0 + w*m0*n0 ]
+					-= *c * C[ ((int)*amo_z-1) + ((int)*amo_r-1) * m0 + w*m0*n0 ];
+                                if (C[ ((int)*amo_z-1) + ((int)*amo_r-1) * m0 + w*m0*n0 ] <= 0) {
+                                        C[ ((int)*amo_z-1) + ((int)*amo_r-1) * m0 + w*m0*n0 ] = 0;
+                                }
+			}
+
+    } // end for-t
+
+
+    /* copy results */
+
+    for (int u = 0; u < (m0+1); u++ ) {
+    	for (int v = 0; v < n0; v++ ) {
+    		for (int w = 0; w < (l0+1); w++) {
+					for (int k = 0; k < 3; k++) {
+						// copy C & Jr
+						if (u < m0 && w < l0) {
+							CNew[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ]
+								= C[ u + v*m0 + w*m0*n0 + k*m0*n0*l0 ];
+							JrNew[ u + v*m2 + w*m2*n2 + k*m2*n2*l2 ]
+								= Jr[ u + v*m2 + w*m2*n2 + k*m2*n2*l2 ];
+						}
+						// copy Jz
+						if (w < l0) {
+							JzNew[ u + v*m1 + w*m1*n1 + k*m1*n1*l1 ]
+								= Jz[ u + v*m1 + w*m1*n1 + k*m1*n1*l1 ];
+						}
+						// copy Jq
+						if (u < m0) {
+							JqNew[ u + v*m3 + w*m3*n3 + k*m3*n3*l3 ]
+								= Jq[ u + v*m3 + w*m3*n3 + k*m3*n3*l3 ];
+						}
+					} // end for-k
+					// copy Ez
+					if (w < l0) {
+						EzNew[ u + v*m1 + w*m1*n1 ]
+							= Ez[ u + v*m1 + w*m1*n1 ];
+					}
+					// copy Er
+					if (u < m0 && w < l0) {
+						ErNew[ u + v*m2 + w*m2*n2 ]
+							= Er[ u + v*m2 + w*m2*n2 ];
+					}
+					// copy Eq
+					if (u < m0) {
+						EqNew[ u + v*m3 + w*m3*n3 ]
+							= Eq[ u + v*m3 + w*m3*n3 ];
+					}
+				} // end for-w
+			} // end for-v
+		} // end for-u
+
+    /* free memory */
+
+}
